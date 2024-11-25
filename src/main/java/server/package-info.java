f8a0d91ddf8439cf7d919f68f4425a8d0e5e9b6d
Bogdan11031999 <84954145+Bogdan11031999@users.noreply.github.com>
2024-11-25 13:47:48 +0100

    Initial commit

---
+++ src/main/java/server/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Questo package contiene la classe {@code ServerMain} che rappresenta il punto di ingresso del server RMI,
+ * e la classe {@code ServerImplementation} che fornisce un'implementazione dell'interfaccia {@code EmotSongsInterface}.
+ */
+package server;
+
